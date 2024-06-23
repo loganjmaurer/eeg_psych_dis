da296bf767e50dd2dbe6560483e2d5e7a8a3e77d
loganjmaurer <loganmaurer@protonmail.com>
2024-06-22 23:23:20 -0500

    Update eeg.jl

--- eeg.jl
+++ eeg.jl
@@ -1,3 +1,17 @@
-using CSV, DataFrames
+using CSV, DataFrames, StatsPlots, Plots
 
-myData= CSV.File("EEG.machinelearing_data_BRMH.csv") |> DataFrame
+# Read data, treating "NA" as missing, then remove missing
+myData = CSV.File("EEG.machinelearing_data_BRMH.csv", missingstrings=["NA"]) |> DataFrame
+dropmissing!(myData, [:IQ, :education])
+
+# Get the unique levels of main.disorder
+main_disorders = unique(myData[!, "main.disorder"])
+
+# Create the violin plot
+plt = plot(figsize=(12, 8), palette=:auto)
+violin!(plt, [myData[myData[!, "main.disorder"] .== disorder, :IQ] for disorder in main_disorders], 
+       label=nothing)
+xlabel!(plt, "Main Disorder")
+ylabel!(plt, "IQ")
+title!(plt, "IQ Distributions by Main Disorder")
+plot!(legend=main_disorders, title="Main Disorder", legendposition=:outertop)
